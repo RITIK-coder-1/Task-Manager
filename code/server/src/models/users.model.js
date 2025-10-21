4e856d8294c69ede447d84aad26ee01900d53448
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-21 15:15:29 +0530

    wrote the first two fields for my user schema

--- code/server/src/models/users.model.js
+++ code/server/src/models/users.model.js
@@ -10,14 +10,38 @@ import mongoose from "mongoose"; // importing mongoose
 // ----------------------------------------------
 
 const userSchema = new mongoose.Schema(
-  {},
+  {
+    // The fullname of the user. The first name is mandatory and the last name is optional.
+    fullName: {
+      firstName: {
+        type: String, // Only string
+        required: true,
+        unique: false, // multiple people can have the same name
+      },
+      lastName: {
+        type: String,
+        required: false, // it is not required
+        unique: false,
+      },
+    },
+
+    // the unique username of the user. This could be a combination of different characters. 6-8 lowercase characters
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      minlength: [6, "You have to enter at least 6 characters"],
+      maxlength: [8, "You can not enter more than 8 characters"],
+    },
+  },
   {
     timestamps: true,
   }
 );
 
 // ----------------------------------------------
-// Creating the User model out of the user schema
+// Creating the User model based on "userSchema"
 // ----------------------------------------------
 
 const User = new mongoose.model("User", userSchema);
