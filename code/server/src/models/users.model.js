34ee25530c34205704b91015a915d068283f0c8a
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-21 15:41:31 +0530

    created the entire user schema with required fields and constraints for my app

--- code/server/src/models/users.model.js
+++ code/server/src/models/users.model.js
@@ -14,25 +14,42 @@ const userSchema = new mongoose.Schema(
     // The fullname of the user. The first name is mandatory and the last name is optional.
     fullName: {
       firstName: {
-        type: String, // Only string
-        required: true,
+        type: String,
+        required: true, // mandatory
         unique: false, // multiple people can have the same name
       },
       lastName: {
         type: String,
-        required: false, // it is not required
+        required: false, // optional
         unique: false,
       },
     },
 
-    // the unique username of the user. This could be a combination of different characters. 6-8 lowercase characters
+    // the unique username of the user. This could be a combination of different characters. 6 lowercase characters
     username: {
       type: String,
       required: true,
-      unique: true,
+      unique: true, // unique
       lowercase: true,
+      minlength: [6, "The username must be of 6 characters only."],
+      maxlength: [6, "The username must be of 6 characters only."],
+    },
+
+    // the password of the user. 6-8 characters
+    password: {
+      type: String,
+      required: true, // mandatory
+      unique: false,
       minlength: [6, "You have to enter at least 6 characters"],
       maxlength: [8, "You can not enter more than 8 characters"],
+    },
+
+    // the email of the User
+    email: {
+      type: String,
+      required: true, // mandatory
+      unique: true, // unique
+      lowercase: true,
     },
   },
   {
