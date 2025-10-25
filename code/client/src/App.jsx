30ed4dd8bee58b57b8b0793f46a71b216b717f8e
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-25 20:54:07 +0530

    I created a temporary test component to test the api req/res between the client and the server

--- code/client/src/App.jsx
+++ code/client/src/App.jsx
@@ -3,31 +3,11 @@ App.jsx
 This is the root application that hosts all the app pages 
 ------------------------------------------------------------------------------ */
 
-import { useEffect, useState } from "react";
 import "./styles/App.css";
-import axios from "axios";
+import Test from "./components/test/Test";
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios.get("/api/v1/test").then((data) => {
-      setData(data.data);
-    });
-  }, []);
-
-  return (
-    <div>
-      {data.map((ele) => {
-        return (
-          <>
-            <h1>{ele.name}</h1>
-            <p>{ele.description}</p>
-            <span>Created By: {ele.createdBy}</span>
-          </>
-        );
-      })}
-    </div>
-  );
+  return <Test />;
 }
 
 export default App;
