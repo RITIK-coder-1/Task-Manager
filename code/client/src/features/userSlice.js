c94a60d619d9b08c0182998a62789a450d1ccf43
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-28 13:32:20 +0530

    I wrote the async thunk and the required cases to register a user

--- code/client/src/features/userSlice.js
+++ code/client/src/features/userSlice.js
@@ -3,7 +3,7 @@ user.features.js
 This is the slice for all the user related global state management
 ------------------------------------------------------------------------------ */
 
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import {
   registerUser,
   loginUser,
@@ -13,5 +13,56 @@ import {
   updatePassword,
   updatePic,
 } from "../services/index.services.js";
+
+/* ---------------------------------------------------------------------------
+The function to register a user
+------------------------------------------------------------------------------ */
+
+const register = createAsyncThunk(
+  "users/register",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await registerUser(userData);
+      return response; // the response sent by the backend
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+/* ---------------------------------------------------------------------------
+The Slice
+------------------------------------------------------------------------------ */
+
+const userSlice = createSlice({
+  name: "users",
+  initialState: {
+    data: null, // will hold the fetched data
+    status: "idle", // idle, pending, succeeded, failed
+    error: null, // will hold any error message
+  },
+  extraReducers: (builder) => {
+    /* ---------------------------------------------------------------------------
+       All the cases for registering a user
+    ------------------------------------------------------------------------------ */
+
+    // the pending case
+    builder.addCase(register.pending, (state) => {
+      (state.status = "pending"), (state.error = null); // clear previous errors
+    });
+
+    // the success case
+    builder.addCase(register.fulfilled, (state, action) => {
+      (state.status = "succeeded"), (state.data = action.payload);
+    });
+
+    // the failure case
+    builder.addCase(register.rejected, (state) => {
+      (state.status = "failed"),
+        (state.error = action.payload || "An unknown error occured"),
+        (state.data = []); // clear data on failure
+    });
+  },
+});
 
 export default userSlice.reducer;
