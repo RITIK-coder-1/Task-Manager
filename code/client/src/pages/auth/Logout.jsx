048c6077400b55780588ef92164e56e45a85c92a
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-31 12:09:17 +0530

    Added a conditional rendering message in the logout component

--- code/client/src/pages/auth/Logout.jsx
+++ code/client/src/pages/auth/Logout.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { Button } from "../../components/index.components";
 import { logout } from "../../features/userSlice.js";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function Logout() {
+  const status = useSelector((state) => state.users.status);
+  const error = useSelector((state) => state.users.error);
   const dispatch = useDispatch();
+  const conditionalMessage = () => {
+    if (status === "pending") {
+      return <span>Working...</span>;
+    } else if (status === "succeeded") {
+      return <span>Successfully logged out!</span>;
+    } else if (status === "failed") {
+      return <span>{error}</span>;
+    }
+  };
   return (
-    <Button
-      content={"Log out"}
-      onClick={() => {
-        dispatch(logout());
-      }}
-    />
+    <>
+      <Button
+        content={"Log out"}
+        onClick={() => {
+          dispatch(logout());
+        }}
+      />
+      {conditionalMessage()}
+    </>
   );
 }
 
